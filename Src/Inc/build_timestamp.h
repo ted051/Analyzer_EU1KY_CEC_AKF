804c4ec3c4d0510a5102e755a87270060e0937e7
Wolfgang Kiefer <woki@onlinehome.de>
2020-11-19 19:51:39 +0100

    Correction (number parameters)

--- Src/Inc/build_timestamp.h
+++ Src/Inc/build_timestamp.h
@@ -1,7 +1,7 @@
 #ifndef BUILD_TIMESTAMP 
-#define HGREV 535b631b
-#define BUILD_TIMESTAMP_US "2020-11-19 17:50 UT"
-#define BUILD_TIMESTAMP_D "19-11-2020 17:50 UT"
+#define HGREV 79ffa546
+#define BUILD_TIMESTAMP_US "2020-11-19 18:50 UT"
+#define BUILD_TIMESTAMP_D "19-11-2020 18:50 UT"
 #define HGREVSTR(s) stringify_(s) 
 #define stringify_(s) #s 
 #endif 
